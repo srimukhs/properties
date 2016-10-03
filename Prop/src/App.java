8f10d9a8335e2b97b8c25c88ac98213c164d4415
srimukhs <sri.mich@gmail.com>
2016-10-03 11:45:27 -0500

    tes

---
+++ Prop/src/App.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class App {
+
+  public static void main(String[] args) {
+	App app = new App();
+	app.printThemAll();
+  }
+
+  private void printThemAll() {
+
+	Properties prop = new Properties();
+	InputStream input = null;
+
+	try {
+
+		String filename = "resource/test.properties";
+		input = getClass().getClassLoader().getResourceAsStream(filename);
+		if (input == null) {
+			System.out.println("Sorry, unable to find " + filename);
+			return;
+		}
+
+		prop.load(input);
+
+		Enumeration<?> e = prop.propertyNames();
+		while (e.hasMoreElements()) {
+			String key = (String) e.nextElement();
+			String value = prop.getProperty(key);
+			System.out.println("Key : " + key + ", Value : " + value);
+		}
+
+	} catch (IOException ex) {
+		ex.printStackTrace();
+	} finally {
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+  }
+
+}
